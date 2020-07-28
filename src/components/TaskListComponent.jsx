b41e8771a0b3ca9dba66820c27fdadcace8332fb
Maria Merkova <maria.h.merkova@gmail.com>
2020-07-28 11:13:26 +0300

    Adding validation on field taslist name with formik

--- src/components/TaskListComponent.jsx
+++ src/components/TaskListComponent.jsx
@@ -1,19 +1,45 @@
 import React, {Component} from "react";
 import PropTypes from 'prop-types';
 import {Button} from "react-bootstrap";
+import {Formik, Form, Field} from 'formik';
+
+function validateRequired(value) {
+    let error;
+    if (value === null || value === '') {
+        error = 'The field is required';
+    }
+    return error;
+}
 
 function TaskListComponent({todoList, onDelete, handleInputChange, onUpdate}) {
-    return(
+    return (
         <article className="task-list">
-            <div className="title">
-                <input type='text' value={todoList.name} onChange={(e) => handleInputChange(e)}/>
-                <Button variant="info" size="sm" className="delete-button" onClick={() => { if (window.confirm('Are you sure you want to delete this item?')) onDelete(todoList.id)}}>Delete</Button>
-            </div>
-            <div>
-                <Button variant="info" size="sm" className="save-button" onClick={() => onUpdate(todoList.id)}>Save</Button>
-            </div>
+            <Formik
+                initialValues={{
+                    name: todoList.name
+                }}
+                onSubmit={values => {
+                    onUpdate(todoList.id)
+                }}
+            >
+                {formik => (
+                    <Form>
+                        <div className="title">
+                            <Field name="name" type='text' validate={validateRequired} value={todoList.name}
+                                   onChange={(e) => {formik.handleChange(e); handleInputChange(e)}}/>
+                            {formik.errors.name && <div>{formik.errors.name}</div>}
+                            <Button variant="info" size="sm" className="delete-button" onClick={() => {
+                                if (window.confirm('Are you sure you want to delete this item?')) onDelete(todoList.id)
+                            }}>Delete</Button>
+                        </div>
+                        <div>
+                            <Button variant="info" size="sm" className="save-button" type="submit">Save</Button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </article>
-        )
+    )
 
 
 }
